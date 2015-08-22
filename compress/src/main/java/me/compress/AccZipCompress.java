c6130df032fd63076ce78835e81c0599deedd83e
whyDK37 <why.DK37@gmail.com>
2015-08-22 10:44:29 +0800

    提交压缩zip方法

---
+++ compress/src/main/java/me/compress/AccZipCompress.java
@@ -0,0 +1,56 @@
+package me.compress;
+
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.zip.Zip64Mode;
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
+import org.apache.commons.compress.utils.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+/**
+ * acc-apache commons compress
+ * Created by hOward on 2015/8/22.
+ */
+public class AccZipCompress implements CompressFile{
+
+    public void compress(String compressPath, String[] sourcePath) throws CompressFileException{
+        ZipArchiveOutputStream zipOutput = null;
+        try {
+            File zipFile = new File(compressPath);
+            zipOutput = (ZipArchiveOutputStream) new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP, new FileOutputStream(zipFile));
+            zipOutput.setEncoding("UTF-8");
+            zipOutput.setUseZip64(Zip64Mode.AsNeeded);
+            for(String filepath : sourcePath){
+                File file = new File(filepath);
+                InputStream in = null;
+                try {
+                    in = new FileInputStream(file);
+                    ZipArchiveEntry entry = new ZipArchiveEntry(file, file.getName());
+                    //zipOutput.createArchiveEntry(logFile, logFile.getName());
+                    zipOutput.putArchiveEntry(entry);
+                    IOUtils.copy(in, zipOutput);
+                    zipOutput.closeArchiveEntry();
+                }finally{
+                    if(in != null){
+                        try {
+                            in.close();
+                        } catch (Exception e) { }
+                    }
+                }
+            }
+            zipOutput.finish();
+            zipOutput.close();
+        }catch(Exception e){
+            if(zipOutput != null){
+                try {
+                    zipOutput.close();
+                } catch (Exception e1) { }
+            }
+            throw new CompressFileException(e);
+        }
+    }
+}
---
+++ compress/src/main/java/me/compress/CompressFileException.java
@@ -0,0 +1,19 @@
+package me.compress;
+
+/**
+ * 文件压缩例外。
+ * Created by hOward on 2015/8/22.
+ */
+public class CompressFileException extends Exception {
+    public CompressFileException(Exception e){
+        super(e);
+    }
+
+    public CompressFileException(String message){
+        super(message);
+    }
+
+    public CompressFileException(String message, Exception e){
+        super(message,e);
+    }
+}
