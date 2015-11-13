d500244bd8d5ee7e3f71032bbb08e9a4b5ab7a91
WangHuanyu <why.DK37@gmail.com>
2015-11-13 12:14:38 +0800

    增加freemarker

---
+++ FreeMarker/src/main/java/me/fm/Product.java
@@ -0,0 +1,23 @@
+package me.fm;
+
+public class Product {
+
+    private String url;
+    private String name;
+
+    public Product(String url, String name) {
+        this.url = url;
+        this.name = name;
+    }
+
+    // As per the JavaBeans spec., this defines the "url" bean property
+    public String getUrl() {
+        return url;
+    }
+
+    // As per the JavaBean spec., this defines the "name" bean property
+    public String getName() {
+        return name;
+    }
+
+}
