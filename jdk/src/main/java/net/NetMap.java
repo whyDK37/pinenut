d7d6f6f0fd1f4fb9c2ff04659b28022f8ba3de8d
why <why.DK37@gmail.com>
2016-05-04 14:02:09 +0800

    net map

---
+++ jdk/src/main/java/net/NetMap.java
@@ -0,0 +1,17 @@
+package net;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * 类似 windows 网络映射
+ * Created by drug on 2016/5/4.
+ */
+public class NetMap {
+    public static void main(String[] args) throws IOException {
+//        URL urlBase=new URL("http://www.jd.com/");
+//        URLConnection urlConnection = urlBase.openConnection();
+//        System.out.println(urlConnection.getContentEncoding());
+    }
+}
