fd94337b38bb5643cb06db714312794b02705331
WangHuanyu <why.DK37@gmail.com>
2016-05-06 23:34:56 +0800

    测试sync

---
+++ jdk/src/main/java/thread/sync/SyncExample.java
@@ -0,0 +1,66 @@
+package thread.sync;
+
+/**
+ * Created by whydk on 2016/5/5.
+ */
+public class SyncExample {
+
+    int a = 0;
+    boolean flag = false;
+
+    public synchronized void write(){
+        System.out.println("write...");
+        a = 1;
+        flag = true;
+        System.out.println("write end.");
+    }
+
+    public synchronized void read(){
+        System.out.println("read...");
+        if(flag){
+            int i = a;
+            System.out.println(i);
+        }
+        System.out.println("read end.");
+    }
+
+    @Override
+    public String toString() {
+        return "SyncExample{" +
+                "a=" + a +
+                ", flag=" + flag +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        final SyncExample syncExample = new SyncExample();
+        Thread write = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("write:"+syncExample.toString());
+                syncExample.write();
+            }
+        });
+
+        Thread read = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("read:"+syncExample.toString());
+                syncExample.read();
+            }
+        });
+
+        Thread sout = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("sout"+i);
+                }
+
+            }
+        });
+        sout.start();
+        write.start();
+        read.start();
+    }
+}
