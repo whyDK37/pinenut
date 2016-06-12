b54fbf020bd1f53c0fe3e148edc5393afb900a39
why <why.DK37@gmail>
2016-06-12 16:36:52 +0800

    hash test

--- redis/src/main/java/demo/StringTest.java
+++ redis/src/main/java/demo/StringTest.java
@@ -6,13 +6,14 @@ import redis.clients.jedis.Jedis;
 /**
  * Created by drug on 2016/4/27.
  */
-public class Test {
+public class StringTest {
     public static void main(String[] args) {
         String host = "127.0.0.1";
         int port = 6379;
+
         Jedis jedis = JedisUtil.createJedis(host,port);
         jedis.del("abc");
-        jedis.append("abc","  abc");
+        jedis.append("abc", "  abc");
         System.out.println(jedis.get("abc"));
 
         jedis.disconnect();
