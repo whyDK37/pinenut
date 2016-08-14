5046baa24855e9d2a81795bbf2065993759a2fe1
wanghuanyu <why.DK37@gmail.com>
2016-08-14 11:09:06 +0800

    modify

--- spring-learn/spring-web/src/main/java/web/controller/HelloController.java
+++ spring-learn/spring-web/src/main/java/web/controller/HelloController.java
@@ -2,6 +2,7 @@ package web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * curl -X POST http://localhost:8080/hello
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
     @RequestMapping("/hello")
+    @ResponseBody
     public String index() {
         return "Greetings from Spring Boot!";
     }
