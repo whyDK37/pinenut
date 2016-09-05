5a50c0df14acba06728bec97410d41f343b9514c
wanghuanyu <why.DK37@gmail.com>
2016-09-05 08:58:45 +0800

    add weak hash map test

--- java-core/jdk/src/test/java/jdk/util/WeakHashMapTest.java
+++ java-core/jdk/src/test/java/jdk/util/WeakHashMapTest.java
@@ -1,36 +1,41 @@
 package jdk.util;
 
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 import java.util.WeakHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by whydk on 2016/8/30.
  */
 public class WeakHashMapTest {
-    static Map wMap = new WeakHashMap();
+    static Map<Long, AtomicInteger> wMap = new WeakHashMap();
 
-    public static void init() {
-        wMap.put("1", "ding");
-        wMap.put("2", "job");
-    }
+    @Test
+    public void testWeakHashMap() throws IOException {
+        File log = new File("D:\\\\log.txt");
+        FileUtils.write(log,"");
+        StringBuilder sb = new StringBuilder(10000);
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            long time = System.currentTimeMillis();
+            AtomicInteger atomicInteger = wMap.get(time);
+            if (atomicInteger == null) {
+                atomicInteger = new AtomicInteger(1);
+                wMap.put(time, atomicInteger);
+            }
 
-    public static void testWeakHashMap() {
-
-        System.out.println("first get:" + wMap.get("1"));
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            sb.append(time + " : " + atomicInteger.getAndAdd(1)).append(" ").append(wMap.size()).append("\n");
+//            System.out.println(time + " : " + atomicInteger.getAndAdd(1));
+//            System.out.println(wMap.size());
+            if(sb.length()>10000){
+                FileUtils.write(log,sb.toString(),true);
+                sb.delete(0,sb.length()-1);
+            }
         }
-        System.out.println("next get:" + wMap.get("1"));
     }
 
-    public static void main(String[] args) {
-        init();
-        testWeakHashMap();
-    }
-
-    public void test() {
-        WeakHashMap weakHashMap = new WeakHashMap();
-    }
 }
