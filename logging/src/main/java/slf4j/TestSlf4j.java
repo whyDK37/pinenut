bc1f82904e7222219d0c6e3fbc9631f6acaeea25
wanghuanyu <why.DK37@gmail.com>
2016-09-12 08:39:52 +0800

    add slf4j demo

--- logging/src/main/java/slf4j/TestSlf4j.java
+++ logging/src/main/java/slf4j/TestSlf4j.java
@@ -11,18 +11,20 @@ public class TestSlf4j {
         PropertyConfigurator.configure(TestSlf4j.class.getClassLoader().getResourceAsStream("log4j.properties"));
 
         Logger logger = LoggerFactory.getLogger(TestSlf4j.class);
-
+        System.out.println("logger : " + logger.getClass());
         logger.info("yes");
 
         org.slf4j.Logger logger2 = LoggerFactory.getLogger(TestSlf4j.class);
 
         logger2.info("yes");
 
-        logger2.debug("hello {}","debe");
+        logger2.debug("hello {}", "debe");
         logger2.warn("hello");
         logger2.trace("hello");
 
         logger.debug("Processing trade with id: {}  and symbol : {} ", 123, "symbol");
+
+        logger.info("","");
 
 
     }
