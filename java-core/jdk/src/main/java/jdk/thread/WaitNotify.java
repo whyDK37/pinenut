4884729e64bea2206d46dfb7327b8c28d96a8f53
wanghuanyu <why.DK37@gmail.com>
2017-03-13 09:01:51 +0800

    modify

--- java-core/jdk/src/main/java/jdk/thread/WaitNotify.java
+++ java-core/jdk/src/main/java/jdk/thread/WaitNotify.java
@@ -22,7 +22,8 @@ public class WaitNotify {
     static class Wait implements Runnable {
 
 
-        public void run() {            // 加锁，拥有lock的Monitor
+        public void run() {
+            // 加锁，拥有lock的Monitor
             synchronized (lock) {
                 // 当条件不满足时，继续wait，同时释放了lock的锁
                 while (flag) {
@@ -31,15 +32,16 @@ public class WaitNotify {
                         lock.wait();
                     } catch (InterruptedException e) {
                     }
-                }     // 条件满足时，完成工作
-
+                }
+                // 条件满足时，完成工作
                 System.out.println(Thread.currentThread() + " flag is false. running     @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
             }
         }
     }
 
     static class Notify implements Runnable {
-        public void run() {              // 加锁，拥有lock的Monitor
+        public void run() {
+            // 加锁，拥有lock的Monitor
             synchronized (lock) {
                 // 获取lock的锁，然后进行通知，通知时不会释放lock的锁，
                 // 直到当前线程释放了lock后，WaitThread才能从wait方法中返回
