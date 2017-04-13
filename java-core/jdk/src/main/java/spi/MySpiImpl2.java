06389df1c8577ef624d3f1dc950b4ca602cd8fb9
wanghuanyu <why.DK37@gmail.com>
2017-04-13 14:56:16 +0800

    spi test

---
+++ java-core/jdk/src/main/java/spi/MySpiImpl2.java
@@ -0,0 +1,11 @@
+package spi;
+
+/**
+ * Created by why on 4/12/2017.
+ */
+public class MySpiImpl2 implements MySpi {
+    @Override
+    public void hello() {
+        System.out.println("hello2....");
+    }
+}
