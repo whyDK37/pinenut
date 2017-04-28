a2b22335bdd1920668d8e70120eeb90ed3e77df6
wanghuanyu <why.DK37@gmail.com>
2017-04-28 18:02:17 +0800

    斐波那契额

--- commons/src/main/java/algorithms/Fibonacci.java
+++ commons/src/main/java/algorithms/Fibonacci.java
@@ -19,8 +19,8 @@ public class Fibonacci {
      * @return
      */
     static int fib(int i) {
-        if (i <= 2) {
-            return 1;
+        if (i < 2) {
+            return i == 0 ? 0 : 1;
         }
         return fib(i - 1) + fib(i - 2);
     }
