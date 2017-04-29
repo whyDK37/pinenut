d69829ca338780d4c71e66bb8703639af52459f0
wanghuanyu <why.DK37@gmail.com>
2017-04-29 10:48:59 +0800

    有界队列

---
+++ commons/src/test/java/data_structures/MyFixArrayQueueTest.java
@@ -0,0 +1,29 @@
+package data_structures;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by why on 4/29/2017.
+ */
+public class MyFixArrayQueueTest {
+    @Test
+    public void testSize() throws Exception {
+        MyFixArrayQueue<String> queue = new MyFixArrayQueue(10);
+
+        queue.add("a");
+        queue.add("b");
+        queue.add("c");
+
+        Assert.assertEquals(queue.peek(), "a");
+        Assert.assertEquals(queue.poll(), "a");
+        Assert.assertEquals(queue.poll(), "b");
+        for (int i = 0; i < 20; i++) {
+            queue.add("" + i);
+            queue.poll();
+        }
+
+        Assert.assertEquals(queue.size(), 1);
+    }
+
+}
