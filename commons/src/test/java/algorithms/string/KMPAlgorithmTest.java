19abac3df4d207476effb97390984c9f0770843a
wanghuanyu <why.DK37@gmail.com>
2017-05-11 11:14:34 +0800

    KMP 算法实现

---
+++ commons/src/test/java/algorithms/string/KMPAlgorithmTest.java
@@ -0,0 +1,25 @@
+package algorithms.string;
+
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by why on 5/11/2017.
+ */
+public class KMPAlgorithmTest {
+    @Test
+    public void testMatchString() throws Exception {
+        String str = "abcabcabcabe";
+        String mode = "ababaaaba";
+
+        int[] ints = KMPAlgorithm.calculateK(mode);
+        System.out.println(Arrays.toString(ints));
+
+        boolean b = KMPAlgorithm.matchString(str, mode);
+        System.out.println(b);
+
+        str.contains(mode);
+    }
+
+}
---
+++ java-core/jdk/src/main/java/concurrency/fj/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * fork/join demo
+ * Created by why on 5/11/2017.
+ */
+package concurrency.fj;
