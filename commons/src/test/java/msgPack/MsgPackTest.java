a0d3cb0c7d23a5001e9fd82d507f32cfc38dd82c
Wang Huanyu <wanghuanyu@imdada.com>
2017-08-22 13:41:30 +0800

    msg pack test

---
+++ commons/src/test/java/msgPack/MsgPackTest.java
@@ -0,0 +1,40 @@
+package msgPack;
+
+import org.msgpack.MessagePack;
+import org.msgpack.template.Templates;
+import org.msgpack.type.Value;
+import org.msgpack.unpacker.Converter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MsgPackTest {
+    public static void main(String[] args) throws IOException {
+        // Create serialize objects.
+        List<String> src = new ArrayList<String>();
+        src.add("msgpack");
+        src.add("kumofs");
+        src.add("viver");
+
+        MessagePack msgpack = new MessagePack();
+// Serialize
+        byte[] raw = msgpack.write(src);
+        System.out.println(raw.length);
+        System.out.println(new String(raw));
+
+// Deserialize directly using a template
+        List<String> dst1 = msgpack.read(raw, Templates.tList(Templates.TString));
+        System.out.println(dst1.get(0));
+        System.out.println(dst1.get(1));
+        System.out.println(dst1.get(2));
+
+// Or, Deserialze to Value then convert type.
+        Value dynamic = msgpack.read(raw);
+        List<String> dst2 = new Converter(dynamic)
+                .read(Templates.tList(Templates.TString));
+        System.out.println(dst2.get(0));
+        System.out.println(dst2.get(1));
+        System.out.println(dst2.get(2));
+    }
+}
