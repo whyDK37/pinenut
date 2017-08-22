6f5773f86186738868aebbacf83546f244131b57
Wang Huanyu <wanghuanyu@imdada.com>
2017-08-22 15:29:47 +0800

    增加测试代码

---
+++ commons/src/test/java/msgPack/MsgPackTest3.java
@@ -0,0 +1,28 @@
+package msgPack;
+
+
+import org.msgpack.core.MessageBufferPacker;
+import org.msgpack.core.MessagePack;
+
+import java.io.IOException;
+
+public class MsgPackTest3 {
+    public static void main(String[] args) throws IOException {
+        // Create serialize objects.
+        Son2 son2 = new Son2();
+        son2.setName("name");
+        son2.setAge(11);
+
+        MessageBufferPacker messageBufferPacker = MessagePack.newDefaultBufferPacker();
+// Serialize
+//        byte[] raw = msgpack.write(son2);
+//
+//// Deserialize directly using a template
+//        System.out.println(msgpack.read(raw,Son2.class).getName());
+//        System.out.println(msgpack.read(raw,Son2.class).getAge());
+//
+//        System.out.println(msgpack.read(raw,Son.class).getName());
+//        System.out.println(msgpack.read(raw,Son.class).getId());
+
+    }
+}
---
+++ commons/src/test/java/msgPack/Son.java
@@ -0,0 +1,41 @@
+package msgPack;
+
+import org.msgpack.annotation.Index;
+
+public class Son {
+
+    /** 编号 */
+    @Index(1)
+    public Integer id;
+    /** 名字 */
+    @Index(2)
+    public String name;
+    /**身高*/
+    @Index(3)
+    public Double height;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+}
