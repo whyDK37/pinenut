537075369d58a28e2860f1a089e905b49e0f2a98
wanghuanyu <why.DK37@gmail.com>
2017-09-04 21:22:09 +0800

    test

--- commons/src/main/java/org/commons/collections4/CaseInsensitiveMapTest.java
+++ commons/src/main/java/org/commons/collections4/CaseInsensitiveMapTest.java
@@ -2,16 +2,18 @@ package org.commons.collections4;
 
 import org.apache.commons.collections4.map.CaseInsensitiveMap;
 
+import java.util.Map;
+
 /**
  * 忽略key大小写的map
  */
 public class CaseInsensitiveMapTest {
 
   public static void main(String[] args) {
-    CaseInsensitiveMap<String, String> map = new CaseInsensitiveMap();
+    Map<String, String> map = new CaseInsensitiveMap<>();
 
-    map.put("one", "one");
     map.put("ONE", "one");
+    map.put("one", "one");
 
     System.out.println(map.size());
   }
