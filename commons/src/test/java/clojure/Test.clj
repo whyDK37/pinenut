16b46738a4e7457b9ed80d5c01f0e1a6a2e75f43
Wang Huanyu <wanghuanyu@imdada.com>
2017-10-09 18:19:12 +0800

    demo

---
+++ commons/src/test/java/clojure/Test.clj
@@ -0,0 +1,54 @@
+val
+
+VALID_GRADES
+
+=
+
+Set
+
+("A", "B", "C", "D", "F")
+
+def
+
+letterGrade
+
+(value:Any)
+
+:String
+
+=
+
+value
+
+match
+
+{case            x
+ :Int            if
+ (90 to 100)     .contains
+ (x)             =>
+ "A"
+ case
+ x               :Int
+ if              (80 to 90)
+ .contains       (x)
+ =>              "B"
+ case            x
+ :Int            if
+ (70 to 80)      .contains
+ (x)             =>
+ "C"
+ case
+ x               :Int
+ if              (60 to 70)
+ .contains       (x)
+ =>              "D"
+ case            x
+ :Int            if
+ (0 to 60)       .contains
+ (x)             =>
+ "F"
+ case
+ x               :String
+ if              VALID_GRADES
+ (x.toUpperCase) =>
+ x.toUpperCase}
