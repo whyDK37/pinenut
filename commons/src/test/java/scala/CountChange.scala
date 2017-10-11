f4826a295e5f43881e2c491d2ae1cb6c730a3144
Wang Huanyu <wanghuanyu@imdada.com>
2017-10-11 15:37:04 +0800

    换零钱问题

--- commons/src/test/java/scala/CountChange.scala
+++ commons/src/test/java/scala/CountChange.scala
@@ -7,17 +7,28 @@ package scala
 object CountChange {
 
   def countChange(money: Int, coins: List[Int]): Int = {
+    // 首先确定边界条件，如果要兑换的钱数为 0，那么返回 1，即只有一种兑换方法：没法兑换。
+    // 这里要注意的是该问题计算所有的兑换方法，无法兑换也算一种方法。
     if (money == 0)
       1
+    //如果零钱种类为 0 或钱数小于 0，没有任何方式进行兑换，返回 0。
     else if (coins.isEmpty || money < 0)
       0
     else
+    //      我们可以把找零的方法分为两类：使用不包含第一枚硬币（零钱）所有的零钱进行找零，使用包含第一枚硬币（零钱）的所有零钱进行找零，
+    // 两者之和即为所有的找零方式。
+    // 第一种找零方式总共有 countChange(money, coins.tail)种，
+    // 第二种找零方式等价为对于 money – conins.head进行同样的兑换，
+    // 则这种兑换方式有 countChange(money - coins.head, coins)种，两者之和即为所有的零钱兑换方式。
       countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 
   def main(args: Array[String]): Unit = {
-    val coins: List[Int] = List(1, 5, 10, 50, 100)
+    val coins: List[Int] = List(1, 2, 5, 10, 50, 100)
     val i = countChange(5, coins)
     print(i)
+
+    print("\n")
+    print(coins.tail)
   }
 }
--- commons/src/test/java/scala/Test.scala
+++ commons/src/test/java/scala/Test.scala
@@ -1,10 +1,52 @@
 package scala
 
+import scala.annotation.tailrec
+
 /**
   * 换零钱问题
   * https://www.ibm.com/developerworks/cn/java/j-lo-funinscala1/
   */
 object Test {
+
+  //xs.head 返回列表里的头元素，即第一个元素
+  //xs.tail 返回除头元素外的剩余元素组成的列表
+  def sum(xs: List[Int]): Int =
+  if (xs.isEmpty) 0 else xs.head + sum(xs.tail)
+
+  //  求最大值
+  def max(xs: List[Int]): Int = {
+    if (xs.isEmpty)
+      throw new java.util.NoSuchElementException
+    if (xs.size == 1)
+      xs.head
+    else if (xs.head > max(xs.tail)) xs.head else max(xs.tail)
+  }
+
+  //  反转字符串
+  def reverse(xs: String): String =
+    if (xs.length == 1) xs else reverse(xs.tail) + xs.head
+
+  //  快速排序
+  def quickSort(xs: List[Int]): List[Int] = {
+    if (xs.isEmpty) xs
+    else
+      quickSort(xs.filter(x => x < xs.head)) ::: xs.head :: quickSort(xs.filter(x => x > xs.head))
+  }
+
+  //  递归求阶乘
+  def factorial(n: Int): Int =
+    if (n == 0) 1 else n * factorial(n - 1)
+
+  //  尾递归求阶乘
+  def factorial2(n: Int): Int = {
+    @tailrec
+    def loop(acc: Int, n: Int): Int =
+      if (n == 0) acc else loop(n * acc, n - 1)
+
+    loop(1, n)
+  }
+
   def main(args: Array[String]): Unit = {
+
   }
 }
