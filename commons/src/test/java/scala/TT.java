bbf6946b6685569b79a4b947ae29f2834b99ec92
Wang Huanyu <wanghuanyu@imdada.com>
2017-10-24 11:16:06 +0800

    sizeOf

---
+++ commons/src/test/java/scala/TT.java
@@ -0,0 +1,7 @@
+package scala;
+
+public class TT {
+    public static void main(String[] args) {
+        HelloWorld.main(args);
+    }
+}
--- java-core/jdk/src/test/java/jdk/util/SizeOfTest.java
+++ java-core/jdk/src/test/java/jdk/util/SizeOfTest.java
@@ -1,28 +1,34 @@
 package jdk.util;//package jdk.me.util;
 //
-//import net.sourceforge.sizeof.SizeOf;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// * start the jvm with the argument: -javaagent:<path to>/SizeOf.jar
-// */
-//public class SizeOfTest {
-//    public static void main(String[] args) {
-////        System.out.println(SizeOf.skipStaticField(true));
-////        System.out.println(SizeOf.setMinSizeToLog(10);
-//
-//        System.out.println(SizeOf.sizeOf(new MyInteger(1)));
-//        System.out.println(SizeOf.sizeOf(1));
-//
-//        //collection
-//        System.out.println("collection & array size");
-//        int[] ia = new int[1];
-//        List<MyInteger> il = new ArrayList<>(1);
-//        System.out.println(SizeOf.deepSizeOf(ia));
-//        System.out.println(SizeOf.deepSizeOf(il));
-//        //calculate object size
-////        SizeOf.iterativeSizeOf(<your object>)
-//    }
-//}
+
+import jdk.lang.MyInteger;
+import net.sourceforge.sizeof.SizeOf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * start the jvm with the argument: -javaagent:D:\workspace\mygit\pinenut\java-core\jdk\libs\SizeOf.jar
+ */
+public class SizeOfTest {
+    public static void main(String[] args) {
+//        System.out.println(SizeOf.skipStaticField(true));
+//        System.out.println(SizeOf.setMinSizeToLog(10);
+
+        System.out.println(SizeOf.sizeOf(new MyInteger(1)));
+        System.out.println(SizeOf.sizeOf((short) 1));
+        System.out.println(SizeOf.sizeOf(1));
+        System.out.println(SizeOf.sizeOf(1L));
+        System.out.println(SizeOf.sizeOf(1.2));
+
+        //collection
+        System.out.println("collection & array size");
+        int[] ia = new int[1];
+        List<MyInteger> il = new ArrayList<>(1);
+        System.out.println(SizeOf.deepSizeOf(ia));
+        System.out.println(SizeOf.deepSizeOf(il));
+        System.out.println(SizeOf.deepSizeOf(new ArrayList<>(100)));
+        //calculate object size
+//        SizeOf.iterativeSizeOf(<your object>)
+    }
+}
