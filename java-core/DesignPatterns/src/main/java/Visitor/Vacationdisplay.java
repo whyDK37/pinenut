a7d1b15544af73e98523b87fefdbd37e873cd7b4
Wang Huanyu <wanghuanyu@imdada.cn>
2018-04-16 14:32:40 +0800

    visitor demo

--- java-core/DesignPatterns/src/main/java/Visitor/Vacationdisplay.java
+++ java-core/DesignPatterns/src/main/java/Visitor/Vacationdisplay.java
@@ -9,77 +9,77 @@ import java.awt.event.ActionListener;
 
 public class Vacationdisplay extends JxFrame
         implements ActionListener {
-  JawtList empList;
-  JTextField total, btotal;
-  JButton Vac;
-  Employee[] employees;
+    JawtList empList;
+    JTextField total, btotal;
+    JButton Vac;
+    Employee[] employees;
 
-  //-----------------------------------------
-  public Vacationdisplay() {
-    super("Vacation Display");
-    JPanel jp = new JPanel();
-    getContentPane().add(jp);
-    jp.setLayout(new GridLayout(1, 2));
-    empList = new JawtList(10);
-    jp.add(empList);
+    //-----------------------------------------
+    public Vacationdisplay() {
+        super("Vacation Display");
+        JPanel jp = new JPanel();
+        getContentPane().add(jp);
+        jp.setLayout(new GridLayout(1, 2));
+        empList = new JawtList(10);
+        jp.add(empList);
 
-    createEmployees();
+        createEmployees();
 
-    Box box = Box.createVerticalBox();
-    jp.add(box);
-    total = new JTextField(5);
-    total.setHorizontalAlignment(JTextField.CENTER);
-    box.add(total);
-    box.add(Box.createVerticalStrut(10));
-    btotal = new JTextField(5);
-    btotal.setHorizontalAlignment(JTextField.CENTER);
-    box.add(btotal);
-    box.add(Box.createVerticalStrut(10));
-    Vac = new JButton("Vacations");
-    box.add(Vac);
-    Vac.addActionListener(this);
-    setSize(300, 200);
-    setVisible(true);
-    total.setText("  ");
-    total.setBackground(Color.white);
+        Box box = Box.createVerticalBox();
+        jp.add(box);
+        total = new JTextField(5);
+        total.setHorizontalAlignment(JTextField.CENTER);
+        box.add(total);
+        box.add(Box.createVerticalStrut(10));
+        btotal = new JTextField(5);
+        btotal.setHorizontalAlignment(JTextField.CENTER);
+        box.add(btotal);
+        box.add(Box.createVerticalStrut(10));
+        Vac = new JButton("Vacations");
+        box.add(Vac);
+        Vac.addActionListener(this);
+        setSize(300, 200);
+        setVisible(true);
+        total.setText("  ");
+        total.setBackground(Color.white);
 
-  }
-
-  //-----------------------------------------
-  static public void main(String argv[]) {
-    new Vacationdisplay();
-  }
-
-  //-----------------------------------------
-  public void createEmployees() {
-    employees = new Employee[7];
-    int i = 0;
-    employees[i++] = new Employee("Susan Bear", 55000, 12, 1);
-    employees[i++] = new Employee("Adam Gehr", 150000, 9, 0);
-    employees[i++] = new Employee("Fred Harris", 50000, 15, 2);
-    employees[i++] = new Employee("David Oakley", 57000, 12, 2);
-    employees[i++] = new Employee("Larry Thomas", 100000, 20, 6);
-    Boss b = new Boss("Leslie Susi", 175000, 16, 4);
-    b.setBonusDays(12);
-    Boss b1 = new Boss("Laurence Byerly", 35000, 17, 6);
-    b1.setBonusDays(17);
-    employees[i++] = b;
-    employees[i++] = b1;
-
-    for (i = 0; i < employees.length; i++) {
-      empList.add(employees[i].getName());
     }
-  }
 
-  //-----------------------------------------
-  public void actionPerformed(ActionEvent e) {
-    Vacationvisitor vac = new Vacationvisitor();
-    Bvacationvisitor bvac = new Bvacationvisitor();
-    for (int i = 0; i < employees.length; i++) {
-      employees[i].accept(vac);
-      employees[i].accept(bvac);
+    //-----------------------------------------
+    static public void main(String argv[]) {
+        new Vacationdisplay();
     }
-    total.setText(new Integer(vac.getTotalDays()).toString());
-    btotal.setText(new Integer(bvac.getTotalDays()).toString());
-  }
+
+    //-----------------------------------------
+    public void createEmployees() {
+        employees = new Employee[7];
+        int i = 0;
+        employees[i++] = new Employee("Susan Bear", 55000, 12, 1);
+        employees[i++] = new Employee("Adam Gehr", 150000, 9, 0);
+        employees[i++] = new Employee("Fred Harris", 50000, 15, 2);
+        employees[i++] = new Employee("David Oakley", 57000, 12, 2);
+        employees[i++] = new Employee("Larry Thomas", 100000, 20, 6);
+        Boss b = new Boss("Leslie Susi", 175000, 16, 4);
+        b.setBonusDays(12);
+        Boss b1 = new Boss("Laurence Byerly", 35000, 17, 6);
+        b1.setBonusDays(17);
+        employees[i++] = b;
+        employees[i++] = b1;
+
+        for (i = 0; i < employees.length; i++) {
+            empList.add(employees[i].getName());
+        }
+    }
+
+    //-----------------------------------------
+    public void actionPerformed(ActionEvent e) {
+        Vacationvisitor vac = new Vacationvisitor();
+        Bvacationvisitor bvac = new Bvacationvisitor();
+        for (int i = 0; i < employees.length; i++) {
+            employees[i].accept(vac);
+            employees[i].accept(bvac);
+        }
+        total.setText(Integer.toString(vac.getTotalDays()));
+        btotal.setText(Integer.toString(bvac.getTotalDays()));
+    }
 }
---
+++ java-core/DesignPatterns/src/main/java/Visitor/t1/EmployeeVisitor.java
@@ -0,0 +1,165 @@
+package Visitor.t1;
+
+import java.util.*;
+
+public class EmployeeVisitor {
+
+}
+
+
+//员工类：抽象元素类  
+interface Employee {
+    public void accept(Department handler); //接受一个抽象访问者访问  
+}
+
+//全职员工类：具体元素类  
+class FulltimeEmployee implements Employee {
+    private String name;
+    private double weeklyWage;
+    private int workTime;
+
+    public FulltimeEmployee(String name, double weeklyWage, int workTime) {
+        this.name = name;
+        this.weeklyWage = weeklyWage;
+        this.workTime = workTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeeklyWage(double weeklyWage) {
+        this.weeklyWage = weeklyWage;
+    }
+
+    public void setWorkTime(int workTime) {
+        this.workTime = workTime;
+    }
+
+    public String getName() {
+        return (this.name);
+    }
+
+    public double getWeeklyWage() {
+        return (this.weeklyWage);
+    }
+
+    public int getWorkTime() {
+        return (this.workTime);
+    }
+
+    public void accept(Department handler) {
+        handler.visit(this); //调用访问者的访问方法  
+    }
+}
+
+//兼职员工类：具体元素类  
+class ParttimeEmployee implements Employee {
+    private String name;
+    private double hourWage;
+    private int workTime;
+
+    public ParttimeEmployee(String name, double hourWage, int workTime) {
+        this.name = name;
+        this.hourWage = hourWage;
+        this.workTime = workTime;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHourWage(double hourWage) {
+        this.hourWage = hourWage;
+    }
+
+    public void setWorkTime(int workTime) {
+        this.workTime = workTime;
+    }
+
+    public String getName() {
+        return (this.name);
+    }
+
+    public double getHourWage() {
+        return (this.hourWage);
+    }
+
+    public int getWorkTime() {
+        return (this.workTime);
+    }
+
+    public void accept(Department handler) {
+        handler.visit(this); //调用访问者的访问方法  
+    }
+}
+
+//部门类：抽象访问者类  
+abstract class Department {
+    //声明一组重载的访问方法，用于访问不同类型的具体元素
+    public abstract void visit(FulltimeEmployee employee);
+
+    public abstract void visit(ParttimeEmployee employee);
+}
+
+//财务部类：具体访问者类  
+class FADepartment extends Department {
+    //实现财务部对全职员工的访问  
+    public void visit(FulltimeEmployee employee) {
+        int workTime = employee.getWorkTime();
+        double weekWage = employee.getWeeklyWage();
+        if (workTime > 40) {
+            weekWage = weekWage + (workTime - 40) * 100;
+        } else if (workTime < 40) {
+            weekWage = weekWage - (40 - workTime) * 80;
+            if (weekWage < 0) {
+                weekWage = 0;
+            }
+        }
+        System.out.println("正式员工" + employee.getName() + "实际工资为：" + weekWage + "元。");
+    }
+
+    //实现财务部对兼职员工的访问  
+    public void visit(ParttimeEmployee employee) {
+        int workTime = employee.getWorkTime();
+        double hourWage = employee.getHourWage();
+        System.out.println("临时工" + employee.getName() + "实际工资为：" + workTime * hourWage + "元。");
+    }
+}
+
+//人力资源部类：具体访问者类  
+class HRDepartment extends Department {
+    //实现人力资源部对全职员工的访问  
+    public void visit(FulltimeEmployee employee) {
+        int workTime = employee.getWorkTime();
+        System.out.println("正式员工" + employee.getName() + "实际工作时间为：" + workTime + "小时。");
+        if (workTime > 40) {
+            System.out.println("正式员工" + employee.getName() + "加班时间为：" + (workTime - 40) + "小时。");
+        } else if (workTime < 40) {
+            System.out.println("正式员工" + employee.getName() + "请假时间为：" + (40 - workTime) + "小时。");
+        }
+    }
+
+    //实现人力资源部对兼职员工的访问  
+    public void visit(ParttimeEmployee employee) {
+        int workTime = employee.getWorkTime();
+        System.out.println("临时工" + employee.getName() + "实际工作时间为：" + workTime + "小时。");
+    }
+}
+
+//员工列表类：对象结构  
+class EmployeeList {
+    //定义一个集合用于存储员工对象  
+    private ArrayList<Employee> list = new ArrayList<Employee>();
+
+    public void addEmployee(Employee employee) {
+        list.add(employee);
+    }
+
+    //遍历访问员工集合中的每一个员工对象  
+    public void accept(Department handler) {
+        for (Object obj : list) {
+            ((Employee) obj).accept(handler);
+        }
+    }
+}  
