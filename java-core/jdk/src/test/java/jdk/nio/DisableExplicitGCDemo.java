2ced231b04d43584509b8219d3d410de7170b621
Wang Huanyu <wanghuanyu@imdada.cn>
2018-06-09 17:43:12 +0800

    DisableExplicitGCDemo.java

--- java-core/jdk/src/test/java/jdk/nio/DisableExplicitGCDemo.java
+++ java-core/jdk/src/test/java/jdk/nio/DisableExplicitGCDemo.java
@@ -4,15 +4,16 @@ import java.nio.ByteBuffer;
 
 /**
  * -XX:MaxDirectMemorySize=10m -XX:+PrintGC -XX:+DisableExplicitGC
- * -Xmx10m -Xms10m -XX:+UseG1GC
+ * -Xmx10m -Xms10m -XX:+UseG1GC -XX:+PrintGCDetails -XX:+PrintHeapAtGC -XX:+PrintGCDateStamps -XX:+PrintAdaptiveSizePolicy -XX:+PrintReferenceGC -verbose:gc -Xloggc:d:\gc.log
  * http://breezylee.iteye.com/blog/2043056
  */
 public class DisableExplicitGCDemo {
-  public static void main(String[] args) {
-    System.out.println("starting");
-    for (int i = 0; i < 100000; i++) {
-      ByteBuffer.allocateDirect(128);
+    public static void main(String[] args) {
+        System.out.println("starting");
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            ByteBuffer.allocateDirect(4024);
+            System.out.println(i);
+        }
+        System.out.println("Done");
     }
-    System.out.println("Done");
-  }
 }  
